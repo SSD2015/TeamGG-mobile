5523504946005f6e3238b017b0363bdea6c2d891
tan_dooky <tan_dooky@windowslive.com>
2015-02-25 14:02:12 +0700

    loginlogout

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -6,38 +6,68 @@ angular.module('starter.controllers', [])
 		$rootScope.bestVoteStatus = false;
 		$state.go("base.projectlist");
 	};
-	$scope.showAlert = function() {
+	$scope.showAlert1 = function() {
 	   var alertPopup = $ionicPopup.alert({
 	     title: 'Login as Guest',
 	     template: 'You can only watch the application'
 	   });
 	   alertPopup.then(function(res) {
-	     $scope.username = "Guest";
-	     $scope.submit();
+	     $rootScope.username = "Guest";
+	     $rootScope.bestVoteStatus = false;
+	     $state.go("base.projectlist");
 	   });
 	};
+
+	$scope.showAlert2 = function(){
+		var alertPopup = $ionicPopup.alert({
+			title: "Please Enter Username",
+		});
+	} 
 })
 
-.controller('ProjectListCtrl', function( $state, $stateParams ){
-	
+.controller('ProjectListCtrl', function( $state, $stateParams, $scope, $rootScope ){
+	if( $rootScope.username == "" || $rootScope.username == "Guest"){
+		$rootScope.username = "Guest"
+		$scope.tagName = "Sign In";
+	}
+	else{
+		$scope.tagName  = "Log Out";
+	}
+	$scope.logout = function(){
+		$state.go("base.login");
+	}
+	$scope.in_out = function(){
+		if( $rootScope.username == "Guest" ){
+			$state.go("base.login");
+		}	
+		else{
+			$scope.logout();
+		}
+	}
 })
 
 .controller('ProjectInfoCtrl', function($stateParams, $scope, $ionicPopup, $rootScope){
 	$scope.id = $stateParams.id;
+	$scope.isBestVoted = false; //Should be read from database
 
 	$scope.changeClass = function(){
-   		$rootScope.bestVoteStatus = !$rootScope.bestVoteStatus;
+   		$scope.isBestVoted = !$scope.isBestVoted;
     };
 
     $scope.confirmVote = function(){
-    	if($rootScope.bestVoteStatus == false ){
+    	if( $scope.isBestVoted == false && $rootScope.bestVoteStatus == false ){
     		$scope.showConfirm1();
     	}
-    	else{
+    	else if( $scope.isBestVoted == false && $rootScope.bestVoteStatus == true ){
     		$scope.showConfirm2();
+
+    	}
+    	else{
+    		$scope.showConfirm3();
     	}
     };
 
+    //false false
 	$scope.showConfirm1 = function() {
 	   var confirmPopup = $ionicPopup.confirm({
 	     		title: 'Vote this Application',
@@ -46,6 +76,7 @@ angular.module('starter.controllers', [])
 	   confirmPopup.then(function(res) {
 	     if(res) {
 	   		$scope.changeClass();
+	   		$rootScope.bestVoteStatus = true;
 	     }
 	   });
  	};
@@ -53,11 +84,24 @@ angular.module('starter.controllers', [])
  	$scope.showConfirm2 = function() {
 	   var confirmPopup = $ionicPopup.confirm({
 	     		title: 'Vote this Application',
-	     		template: 'Are you sure '
+	     		template: 'Are you sure you want to change vote from ... to ... '
 	   });
 	   confirmPopup.then(function(res) {
 	     if(res) {
 	   		$scope.changeClass();
+	     }
+	   });
+ 	};
+
+ 	$scope.showConfirm3 = function() {
+	   var confirmPopup = $ionicPopup.confirm({
+	     		title: 'Cancel vote  this Application',
+	     		template: 'Are you sure you want to cancel '
+	   });
+	   confirmPopup.then(function(res) {
+	     if(res) {
+	   		$scope.changeClass();
+	   		$rootScope.bestVoteStatus = false;
 	     }
 	   });
  	};
